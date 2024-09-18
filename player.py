5ba57d0e5c1609a7d11f34f564be3eb6f3edf31a
Parish350 <parishpugazh@gmail.com>
2024-09-18 18:10:19 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,20 +1,27 @@
 import pgzrun
 
 
-WIDTH = 1000
-HEIGHT = 1000
+WIDTH = 500
+HEIGHT = 500
 
 player = Actor("player")
 player.x = WIDTH/2
 player.y = HEIGHT/2
 
+msg = "Click on the Player"
 def draw():
          screen.fill("sky blue")
          player.draw()
+         screen.draw.text(msg,(50,50))
 
+def on_mouse_down(pos):
+ global msg 
+ if player.collidepoint(pos):
+     player.x= random.randint(50,550)
+     player.y= random.randint(50,450)
+     msg = "Player found"
 
-
-
-
+ else:
+      msg = "Player not found"
 
 pgzrun.go()
