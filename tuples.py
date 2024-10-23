37f466feff40137a6f31ac306f401e99d2d84de3
Parish350 <parishpugazh@gmail.com>
2024-10-23 18:13:26 +0100

    tuples

---
+++ tuples.py
@@ -0,0 +1,14 @@
+import random
+#Packing
+Address = (3,'Lisvane','The Paddock','Cardiff','India','Chennai','12')
+Position = 0,0
+Color = (50,50,50) #Unpacking
+x,y = Position
+print(x)
+
+#A tuple can also be created without using paranthesis
+Position = 0,0
+print(Position[0])
+print(Address[3])
+print(Address[0:2])
+#Umutetable
