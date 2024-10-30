a4d005a6fff90376ab5a46582c21f3b043fca21e
Parish350 <parishpugazh@gmail.com>
2024-10-30 17:26:08 +0000

    tuples2.py

---
+++ tuples2.py
@@ -0,0 +1,8 @@
+import random
+#Packing
+print("Teacher decides which group won")
+GroupA = ("A","4","Jul 3rd","London","Sliver")
+GroupB = ("B",6,"Jul 3rd","London","Bronze")
+GroupC = ("C","8","Jul 3rd","London", "Gold")
+GroupD = ("D","10","July 3rd","London", "Metal")
+GroupE = ("E","12","July 3rd","London","Platinum")
