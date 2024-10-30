b732007f2723f648689cc6a1aa7121542e6fefdf
Parish350 <parishpugazh@gmail.com>
2024-10-30 18:03:24 +0000

    spacegame.py

---
+++ spacegame.py
@@ -0,0 +1,17 @@
+import random
+import pgzrun
+
+HEIGHT = 600
+WIDTH = 500
+spaceship = Actor("spaceship")
+
+def draw():
+         screen.blit("space background",(0,0))
+         spaceship.x = random.randint(50,450)
+         spaceship.y = random.randint(50,450)
+
+alienship = Actor("alienship")
+
+def draw():
+         alienship.x = random.randint(50,450)
+         alienship.y = random.randint(50,450)
