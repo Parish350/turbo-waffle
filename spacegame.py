c19e2213680dc6267465d26e64bbc7f770a5b906
Parish350 <parishpugazh@gmail.com>
2024-11-06 17:22:08 +0000

    spacegame.py

--- spacegame.py
+++ spacegame.py
@@ -4,20 +4,19 @@ import pgzrun
 HEIGHT = 600
 WIDTH = 500
 spaceship = Actor("spaceship")
-
+alienship = Actor("alienship")
+alien = Actor("alien")
+spaceship.x = random.randint(50,450)
+spaceship.y = random.randint(50,450)
+        
+alienship.x = random.randint(50,450)
+alienship.y = random.randint(50,450)      
+         
+alien.x = random.randint(50,450)
+alien.y = random.randint(50,450)   
 def draw():
          screen.blit("space background",(0,0))
-         spaceship.x = random.randint(50,450)
-         spaceship.y = random.randint(50,450)
-
-alienship = Actor("alienship")
-
-def draw():
-         alienship.x = random.randint(50,450)
-         alienship.y = random.randint(50,450)
-
-alien = Actor("alien")
-
-def draw():
-         alien.x = random.randint(50,450)
-         alien.y = random.randint(50,450)         
+         spaceship.draw()
+         alien.draw()
+         alienship.draw()
+pgzrun.go()      
