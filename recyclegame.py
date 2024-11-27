3ca2acc5455e2cbb60ecb66ca97ea43adde583fa
Parish350 <parishpugazh@gmail.com>
2024-11-27 17:56:03 +0000

    Update recyclegame.py

--- recyclegame.py
+++ recyclegame.py
@@ -13,8 +13,22 @@ Final_level = len(images)
 game_over = False
 game_complete = False
 def draw ():
-  screen.blit("universe",(0,0)) 
+  screen.blit("universe",(0,0))
+  for player in players:
+     player.draw()
+
+def update():   
+   global players,level
+   if len(players) == 0:
+    make_players()
+    for player in players:
+     duration = 10
+     player.anchor = "center","bottom"
+     animation = animate(player, duration = duration, y = HEIGHT)
 
 def make_players():
   global players,images,level
+  player = Actor("bag")
+  player.x = WIDTH/(level+1)
+  players.append(player)  
 pgzrun.go()  
