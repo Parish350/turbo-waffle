33b428ac6f752db45f8759694813216796cf624c
Parish350 <parishpugazh@gmail.com>
2024-12-04 17:59:50 +0000

    Update recyclegame.py

--- recyclegame.py
+++ recyclegame.py
@@ -25,10 +25,31 @@ def update():
      duration = 10
      player.anchor = "center","bottom"
      animation = animate(player, duration = duration, y = HEIGHT)
+     animations.append(animation)
 
 def make_players():
   global players,images,level
   player = Actor("bag")
-  player.x = WIDTH/(level+1)
-  players.append(player)  
+  player.x = WIDTH/(level+2)
+  players.append(player)
+  for i in range(level):
+    option = random.choice(images)
+    plr = Actor(option)
+    plr.x = (i+1)*(WIDTH/(level+1))
+    players.append(plr)
+def on_mouse_down(pos):  
+  global players,level,Final_level,animations,game_complete,Score
+  for player in players:
+   if player.collidepoint(pos):
+    if "bag" in player.image:
+      Score += 10
+      if level == Final_level:
+            game_complete = True
+      else:
+          level += 1
+          players=[] 
+          for animation in animations:
+            if animation.running:
+             animation.stop()
+          animations=[]
 pgzrun.go()  
