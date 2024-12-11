7ce743c29f162ab04223b5e65e7ab78b331f272c
Parish350 <parishpugazh@gmail.com>
2024-12-11 17:19:25 +0000

    Update star.py

--- star.py
+++ star.py
@@ -2,14 +2,14 @@ import pgzrun
 import random
 
 HEIGHT = 600
-WIDTH = 1000
+WIDTH = 800
 stars = []
 Score = 0
 level = 1
 players = []
 animations = []
-
-    
+images = ["star"]
+Final_level = 5 
 def update():   
    global stars,level
    if len(stars) == 0:
@@ -19,18 +19,35 @@ def update():
      star.anchor = "center","bottom"
      animation = animate(star, duration = duration, y = HEIGHT)
 
-def make_stars():
-  global stars,images,level
-  star = Actor("star")
-  star.x = WIDTH/(level+1)
-  stars.append(star)  
-
 def draw():
           screen.blit("space background",(0,0)) 
           for star in stars:
             star.draw()
-
-def on_mouse_down():
-        pass   
+  
+def make_stars():
+  global stars,images,level
+  star = Actor("star")
+  star.x = WIDTH/(level+2)
+  stars.append(star)
+  for i in range(level):
+    option = random.choice(images)
+    str = Actor(option)
+    str.x = (i+1)*(WIDTH/(level+1))
+    stars.append(str)
+def on_mouse_down(pos):  
+  global stars,level,Final_level,animations,game_complete,Score
+  for star in stars:
+   if star.collidepoint(pos):
+    if "star" in star.image:
+      Score += 10
+      if level == Final_level:
+            game_complete = True
+      else:
+          level += 1
+          stars=[] 
+          for animation in animations:
+            if animation.running:
+             animation.stop()
+          animations=[]
 
 pgzrun.go()   
