67bf479b0ca70b09f7d5c3d67c7246db78f5e574
Parish350 <parishpugazh@gmail.com>
2025-01-08 17:27:18 +0000

    Update shipanimation.py

--- images/shipanimation.py
+++ images/shipanimation.py
@@ -11,33 +11,36 @@ ship = Actor('ship')
 ship.x = 500
 ship.y = 500
 BLOCK_POSITIONS = [
- (50,50), 
- (750,650),
+ (50,650), 
+ (750,50),
  (50,650),
  (750,50)
 ]
 block_positions = itertools.cycle(BLOCK_POSITIONS)
 def move_block():
  animate(
-  block,
+   block,
   'bounce_end',
   duration = 1,
   pos=next(block_positions)
  )
 
 def move_ship():
-    x = random.randint(750,50)
-    y = random.randint(50,60)
-    ship.target = x,y
+    x = random.randint(50,450)
+    y = random.randint(50,800)
+
+    ship.target = x,y 
+    animate(ship, tween='accel_decel',pos=ship.target, duration = 5, on_finished = move_ship)
+
 def draw():
  screen.fill("blue")
  block.draw()
  ship.draw()
- animation = animate(ship, duration = duration, y = HEIGHT,on_finished = handle_gameover)
+
 
 move_ship()
 clock.schedule_interval(move_ship,2)
 move_block()
 clock.schedule_interval(move_block,2)
-
+ 
 pgzrun.go()
