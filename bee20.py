7b2b6242c0321081aa30ee156b65104869cd4656
Parish350 <parishpugazh@gmail.com>
2025-03-19 17:22:08 +0000

    Update bee20.py

--- bee20.py
+++ bee20.py
@@ -1,7 +1,8 @@
+import random
 import pygame
 pygame.init() 
-WIDTH =  800
-HEIGHT = 600
+WIDTH =  600
+HEIGHT = 500
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 #load the green picture
@@ -16,6 +17,8 @@ flower3x = 400
 flower3y =  100
 flower4x = 100
 flower4y = 100
+SCORE_FONT = pygame.font.SysFont('comicsans',40)
+bee_Score = 0
 
 bee = pygame.image.load("bee.png") 
 bee = pygame.transform.scale(bee,(70,85))
@@ -52,25 +55,46 @@ class Bee(pygame.sprite.Sprite):
 
 run = True
 while run:
- screen.fill("White") 
- screen.blit(img,(0,0))
- screen.blit(bee,(BEE.x,BEE.y))
- screen.blit(flower1,(FLOWER1.x,FLOWER1.y))
- screen.blit(flower2,(FLOWER2.x,FLOWER2.y))
- screen.blit(flower3,(FLOWER3.x,FLOWER3.y))
- screen.blit(flower4,(FLOWER4.x,FLOWER4.y))
- for event in pygame.event.get():
-  if event.type == pygame.QUIT:
-   pygame.quit() 
-  if event.type == pygame.KEYDOWN:
+    screen.fill("White") 
+    screen.blit(img,(0,0))
+    screen.blit(bee,(BEE.x,BEE.y))
+    Bee_score = SCORE_FONT.render("Score:" +str(bee_Score),1,"white") 
+    screen.blit(Bee_score,(400,50))
+    screen.blit(flower1,(FLOWER1.x,FLOWER1.y))
+    screen.blit(flower2,(FLOWER2.x,FLOWER2.y))
+    screen.blit(flower3,(FLOWER3.x,FLOWER3.y))
+    screen.blit(flower4,(FLOWER4.x,FLOWER4.y))
+    for event in pygame.event.get():
+     if event.type == pygame.QUIT:
+      pygame.quit() 
+     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_UP:
-            BEE.y -= 5
+           BEE.y -= 10
         if event.key == pygame.K_DOWN:
-            BEE.y += 5
+            BEE.y += 10
         if event.key == pygame.K_RIGHT:   
-            BEE.x += 5
+            BEE.x += 10
         if event.key == pygame.K_LEFT:
-           BEE.x -= 5 
+            BEE.x -= 10
+
+    if BEE.colliderect(FLOWER1):
+        FLOWER1.x = random.randint(40,WIDTH-85)
+        FLOWER1.y = random.randint(40,HEIGHT-85)
+        bee_Score += 1  
+
+    if BEE.colliderect(FLOWER2):
+        FLOWER2.x = random.randint(40,WIDTH-85)
+        FLOWER2.y = random.randint(40,HEIGHT-85)
+        bee_Score += 1  
+
+    if BEE.colliderect(FLOWER3):
+        FLOWER3.x = random.randint(40,WIDTH-85)
+        FLOWER3.y = random.randint(40,HEIGHT-85)
+        bee_Score += 1  
     
- pygame.display.update()
- 
+    if BEE.colliderect(FLOWER4):
+        FLOWER4.x = random.randint(40,WIDTH-85)
+        FLOWER4.y = random.randint(40,HEIGHT-85)
+        bee_Score += 1   
+
+    pygame.display.update()
