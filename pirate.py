13182af5b13189f751228be0b7eeb40d41c3227e
Parish350 <parishpugazh@gmail.com>
2025-04-09 17:41:59 +0100

    Create pirate.py

---
+++ pirate.py
@@ -0,0 +1,87 @@
+import pygame
+import random
+pygame.init() 
+
+WIDTH =  700
+HEIGHT = 700
+
+img = pygame.image.load("blueyspace.png") 
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+stone1 = pygame.image.load("stone1.png")
+stone1 = pygame.transform.scale(stone1,(50,50))
+stone2 = pygame.image.load("stone2.png")
+stone2 = pygame.transform.scale(stone2,(50,50))
+stone3 = pygame.image.load("stone3.png")
+stone3 = pygame.transform.scale(stone3,(50,50))
+images = [stone1,stone2,stone3]
+font=pygame.font.SysFont("Times New Roman",50) 
+Score = 0
+text=font.render(f"Score:{Score}",True,"yellow")
+
+
+class Pirate(pygame.sprite.Sprite):
+
+   def __init__ (self):
+        super().__init__()     
+        self.image = pygame.image.load("pirate.png")
+        self.image = pygame.transform.scale(self.image,(50,50))
+        self.rect = self.image.get_rect()    
+
+pirateclass = pygame.sprite.Group()
+soldierclass = pygame.sprite.Group()
+gemclass = pygame.sprite.Group()
+
+for i in range(5):
+ pirate1 = Pirate()
+ pirate1.rect.x = random.randint(50,WIDTH-50)
+ pirate1.rect.y = random.randint(50,HEIGHT-50)
+ pirateclass.add(pirate1)
+
+
+class Soldier(pygame.sprite.Sprite):
+
+   def __init__ (self):
+        super().__init__()     
+        self.image = pygame.image.load("soldier.png")
+        self.image = pygame.transform.scale(self.image,(50,50))
+        self.rect = self.image.get_rect()    
+
+for i in range(5):
+ soldier1 = Soldier()
+ soldier1.rect.x = random.randint(50,WIDTH-50)
+ soldier1.rect.y = random.randint(50,HEIGHT-50)
+ soldierclass.add(soldier1)
+
+class Gem(pygame.sprite.Sprite):
+
+   def __init__ (self):
+        super().__init__()     
+        self.image = pygame.image.load("stone1.png")
+        self.image = pygame.transform.scale(self.image,(50,50))
+        self.rect = self.image.get_rect()    
+
+for i in range(9):
+ gem1 = Gem()
+ gem1.rect.x = random.randint(50,WIDTH-50)
+ gem1.rect.y = random.randint(50,HEIGHT-50)
+ gem1.image = random.choice(images)
+ gemclass.add(gem1)
+
+
+run = True
+while run:
+ screen.fill("White") 
+ screen.blit(img,(0,0))
+ screen.blit(text,(50,50)) 
+ for event in pygame.event.get():
+  if event.type == pygame.QUIT:
+   pygame.quit() 
+  if event.type == pygame.MOUSEMOTION: 
+   pos = pygame.mouse.get_pos()
+   pirate1.rect.center = pos 
+
+ pirateclass.draw(screen)
+ soldierclass.draw(screen)
+ gemclass.draw(screen)
+ pygame.display.update()
+
